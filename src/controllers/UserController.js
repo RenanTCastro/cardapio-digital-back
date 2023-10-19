fe1bc77685e1a29ef4ea5745af6d2d14f47002e1
RenanTCastro <renancastrotut@gmail.com>
2023-10-18 23:04:10 -0300

    gerar qr code como pdf finalizado

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const knex = require("../database");
+// const knex = require("../database");
 
 var QRCode = require("qrcode");
 const PDFDocument = require("pdfkit");
@@ -6,22 +6,17 @@ const PDFDocument = require("pdfkit");
 module.exports = {
   async gerarQRCode(req, res, next) {
     try {
-      // const { user_name, user_id } = req.body;
-      // const user_name = "restauranteMillano";
-      // const user_id = "2";
-      // const data = `http://localhost:3001/${user_name}/${user_id}`;
-      const data = "https://ibb.co/2KRLC5g";
+      const { user_name, user_id } = req.body;
+
+      const data = `http://localhost:3001/${user_name}/${user_id}`;
 
       const qrCode = await QRCode.toBuffer(data);
 
       const doc = new PDFDocument({ size: "A4" });
+
       doc.image(qrCode, 125, 250, { width: 350 });
-      doc.end();
-
-      res.setHeader("Content-Type", "application/pdf");
-      res.setHeader("Content-Disposition", 'inline; filename="qrCode.pdf"');
-
       doc.pipe(res);
+      doc.end();
     } catch (error) {
       console.log("Erro ao gerar o QR code e o PDF:", error);
       next(error);
