d3cbe25c8484a5f9cd5aa8677b4cb91eba73a5b2
Alisson Gabriel <eng.alissongabriel@gmail.com>
2023-10-23 20:10:48 -0300

    criação da rota de adição de produto

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,41 @@
+const knex = require("../database");
+
+module.exports = {
+  async adicionaProduto(req, res) {
+    try {
+      await knex("product").insert(req.body);
+      return res.status(201).json({ error: "Produto adicioando com sucesso." });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Erro ao adicionar o novo produto." });
+    }
+  },
+
+  async buscarProdutos(req, res) {
+    try {
+      const result = await knex("product").select("*");
+      return res.json(result);
+    } catch (error) {
+      return res.status(500).json({ error: "Erro ao buscar produtos." });
+    }
+  },
+
+  async editarProduto(req, res) {
+    try {
+      await knex("product").update(req.body).where(req.params);
+      return res.status(200).json({ error: "Produto atualizado com sucesso." });
+    } catch (error) {
+      return res.status(500).json({ error: "Error ao atualizar produto." });
+    }
+  },
+
+  async deletarProduto(req, res) {
+    try {
+      await knex("product").where(req.params).delete();
+      return res.status(200).json({ message: "Produto deletado com sucesso." });
+    } catch (error) {
+      return res.status(500).json({ error: "Error ao deletar produto." });
+    }
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,15 @@ const routes = express.Router();
 
 // Controllers
 const UserController = require("./controllers/UserController");
+const ProductController = require("./controllers/ProductController");
 
 // User routes
 routes.post("/gerar-qr", UserController.gerarQRCode);
+
+// Product routes
+routes.post("/adiciona-produto", ProductController.adicionaProduto);
+routes.put("/atualizar-produto/:product_id", ProductController.editarProduto);
+routes.get("/buscar-produto", ProductController.buscarProdutos);
+routes.delete("/deletar-produto/:product_id", ProductController.deletarProduto);
 
 module.exports = routes;
