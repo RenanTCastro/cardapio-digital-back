44a44d4e0e72e0aba948bba6477d4ea9c19b238d
RenanTCastro <renancastrotut@gmail.com>
2023-11-20 20:46:21 -0300

    deploy

--- knexfile.js
+++ knexfile.js
@@ -11,4 +11,22 @@ module.exports = {
       directory: `${__dirname}/src/database/migrations`,
     },
   },
+
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`,
+    },
+  },
 };
