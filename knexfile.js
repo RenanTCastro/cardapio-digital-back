8d4995c4d53be166739fd3f903b272ba0f89fba6
RenanTCastro <renancastrotut@gmail.com>
2023-11-20 21:09:00 -0300

    deploy

--- knexfile.js
+++ knexfile.js
@@ -15,10 +15,8 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
     },
     pool: {
       min: 2,
