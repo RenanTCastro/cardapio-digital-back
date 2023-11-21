4b19c13fe8c5f699bc6954739a83b9c6d5e1cda0
RenanTCastro <renancastrotut@gmail.com>
2023-11-20 21:44:13 -0300

    deploy

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ const routes = express.Router();
 const UserController = require("./controllers/UserController");
 const ProductController = require("./controllers/ProductController");
 
+// Teste route
+routes.get("/teste", () => res.status(200).json({ ok: "tudo certo" }));
+
 // User routes
 routes.post("/gerar-qr", UserController.gerarQRCode);
 routes.post("/register", UserController.register);
